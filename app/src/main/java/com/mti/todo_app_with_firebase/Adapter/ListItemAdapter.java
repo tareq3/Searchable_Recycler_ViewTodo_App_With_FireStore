8dc82e3bb12866ade7e6008c650f77818bef2868
Tareq3 <mti.tareq3@gmail.com>
2018-06-23 14:06:04 +0600

    Complete Initial commit for master branch

---
+++ app/src/main/java/com/mti/todo_app_with_firebase/Adapter/ListItemAdapter.java
@@ -0,0 +1,166 @@
+/*
+ * Created by Tareq Islam on 6/22/18 10:52 PM
+ *
+ *  Last modified 6/22/18 10:52 PM
+ */
+
+package com.mti.todo_app_with_firebase.Adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import com.mti.todo_app_with_firebase.MainActivity;
+import com.mti.todo_app_with_firebase.R;
+import com.mti.todo_app_with_firebase.model.ToDo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * Created by Tareq on 22,June,2018.
+ */
+
+ class ListItemViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener,View.OnCreateContextMenuListener {
+
+    ItemClickListener mItemClickListener;
+    TextView item_title, item_description;
+
+
+    public ListItemViewHolder(View itemView) {
+        super(itemView);
+
+        item_title = itemView.findViewById(R.id.item_Title);
+        item_description = itemView.findViewById(R.id.item_Description);
+        itemView.setOnClickListener(this);
+        itemView.setOnCreateContextMenuListener(this);
+    }
+
+    //Todo: Need setItemClickListener to use onClick funtion with holder object
+    public void setItemClickListener(ItemClickListener itemClickListener){
+        this.mItemClickListener=itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        mItemClickListener.onClick(v, getAdapterPosition(), false);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Select the action");
+        menu.add(0, 0, getAdapterPosition(), "DELETE");
+    }
+
+}
+
+
+ public class ListItemAdapter extends RecyclerView.Adapter<ListItemViewHolder> implements Filterable {
+
+     MainActivity mMainActivity;
+
+     List<ToDo> mToDoListFiltered;
+     List<ToDo> mToDoList;
+
+     public ListItemAdapter(MainActivity mainActivity, List<ToDo> toDoList) {
+         mMainActivity = mainActivity;
+         mToDoListFiltered = toDoList;
+         mToDoList=toDoList;
+     }
+
+     @NonNull
+        @Override
+        public ListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+         LayoutInflater inflater=LayoutInflater.from(mMainActivity.getBaseContext());
+
+         View view=inflater.inflate(R.layout.list_item,parent,false);
+
+
+            return new ListItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ListItemViewHolder holder, int position) {
+//Todo: Load Data
+
+            //Set data for item
+            holder.item_title.setText(mToDoListFiltered.get(position).getTitle());
+            holder.item_description.setText(mToDoListFiltered.get(position).getDescription());
+
+            holder.setItemClickListener(new ItemClickListener() {
+                @Override
+                public void onClick(View view, int position, boolean isLongClick) {
+                    //when user select item, data will auto set for text view
+                    mMainActivity.title.setText(mToDoListFiltered.get(position).getTitle());
+                    mMainActivity.description.setText(mToDoListFiltered.get(position).getDescription());
+
+                    mMainActivity.isUpdate=true; //Set flag is update -true
+                    mMainActivity.idUpdate= mToDoListFiltered.get(position).getId();
+
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mToDoListFiltered.size();
+        }
+
+        //this method for filtering
+     @Override
+     public Filter getFilter() {
+         return new Filter() {
+             @Override
+             protected FilterResults performFiltering(CharSequence constraint) {
+                 FilterResults filterResults=new FilterResults();
+                 // If the constraint (search string/pattern) is null
+                 // or its length is 0, i.e., its empty then
+                 // we just set the `values` property to the
+                 // original contacts list which contains all of them
+
+
+                 //constraint is the text we have written on searchbox
+                 String charString=constraint.toString();
+                 if(charString.isEmpty() || constraint==null || constraint.length()==0){
+                     mToDoListFiltered=mToDoList;
+                     filterResults.values=mToDoListFiltered;
+                     filterResults.count=mToDoListFiltered.size();
+
+                 }else{
+                     // Some search constraint has been passed
+                     // so let's filter accordingly
+
+                     List<ToDo> filteredList=new ArrayList<>();
+                     // We'll go through all the List and see
+                     // if they contain the supplied string
+                     for(ToDo row : mToDoList){
+                         // name match condition. this might differ depending on your requirement
+                         // here we are looking for title or description match
+                         if (row.getTitle().toLowerCase().contains(charString.toLowerCase())) {
+                             filteredList.add(row);
+                         }
+                     }
+                     mToDoListFiltered=filteredList;
+                     filterResults.values=mToDoListFiltered;
+                 }
+
+                 // Finally set the filtered values
+
+
+                 return filterResults;
+             }
+
+             @Override
+             protected void publishResults(CharSequence constraint, FilterResults results) {
+                mToDoListFiltered= (ArrayList<ToDo>) results.values;
+                notifyDataSetChanged();
+             }
+         };
+     }
+ }
---
+++ app/src/main/java/com/mti/todo_app_with_firebase/MainActivity.java
@@ -0,0 +1,253 @@
+package com.mti.todo_app_with_firebase;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.mti.todo_app_with_firebase.Adapter.ListItemAdapter;
+import com.mti.todo_app_with_firebase.model.ToDo;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.annotation.Nullable;
+
+import dmax.dialog.SpotsDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    List<ToDo> mToDoList=new ArrayList<>();
+
+    FirebaseFirestore mFirestoreDB;
+
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManagerRecyler;
+
+    FloatingActionButton fab;
+
+    public MaterialEditText title,description; //as need to access from adapter
+    public boolean isUpdate=false; //flag to check is update or is add new
+    public String idUpdate=""; //id of item need to be updated
+    ListItemAdapter mAdapter;
+
+    SpotsDialog mAlertDialog;  //dmax dialog thirdparty
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.support.v7.widget.Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // toolbar fancy stuff
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Search...");
+
+
+
+
+        //Init FireStore
+        mFirestoreDB=FirebaseFirestore.getInstance();
+
+        //view
+        mAlertDialog=new SpotsDialog(this); //third party
+        title=findViewById(R.id.title);
+        description=findViewById(R.id.description);
+        fab=findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add new
+                if(!isUpdate){
+                    setData(title.getText().toString(), description.getText().toString());
+                }else{
+                    updateData(title.getText().toString(),description.getText().toString());
+                }
+            }
+        });
+
+        mRecyclerView=findViewById(R.id.listTodo);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManagerRecyler=new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManagerRecyler);
+
+        loadData();
+    }
+
+    private SearchView mSearchView;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        //Associate searchable configuration with the Searchview
+        SearchManager searchManager= (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        mSearchView= (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        mSearchView.setMaxWidth(Integer.MAX_VALUE);
+
+        //Listening to serach queary changes
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //filter recycler view when querry submitted
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                //filter recycler view when text is changed
+                mAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // close search view on back button pressed
+        if (!mSearchView.isIconified()) {
+            mSearchView.setIconified(true);
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getTitle().equals("DELETE"))
+            deleteItem(item.getOrder());
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteItem(int index) {
+        mFirestoreDB.collection("ToDoList")
+                .document(mToDoList.get(index).getId())
+                    .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        loadData();
+                    }
+                });
+    }
+
+    private void updateData(String s_title, String s_description) {
+        mFirestoreDB.collection("ToDoList").document(idUpdate)
+                .update("title",s_title,
+                        "description",s_description).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(MainActivity.this, "Update !", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Realtime Update refresh data
+        mFirestoreDB.collection("ToDoList").document(idUpdate).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                loadData();
+            }
+        });
+    }
+
+    private void setData(String s_title, String s_description) {
+        //Random Id
+        String id= UUID.randomUUID().toString();
+        Map<String, Object> todo=new HashMap<>();
+
+        todo.put("id", id);
+        todo.put("title", s_title);
+        todo.put("description", s_description);
+
+        mFirestoreDB.collection("ToDoList").document(id).set(todo).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                //refresh data
+                loadData();
+            }
+        });
+    }
+
+    private void loadData() {
+        mAlertDialog.show();
+        if(mToDoList.size()>0)
+            mToDoList.clear(); //remove old value
+
+        mFirestoreDB.collection("ToDoList")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                for(DocumentSnapshot documentSnapshot : task.getResult())
+                                {
+                                    ToDo toDo=new ToDo(documentSnapshot.getString("id"),
+                                            documentSnapshot.getString("title"),
+                                            documentSnapshot.getString("description"));
+
+                                    mToDoList.add(toDo);
+                                }
+
+                                mAdapter= new ListItemAdapter(MainActivity.this,mToDoList);
+                                mRecyclerView.setAdapter(mAdapter);
+
+                                mAlertDialog.dismiss();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
